b62717448601e5ba91aa9a25ab9781a9255667e4
Prrofessor <divyanshyadav23457@gmail.com>
2023-01-21 02:35:30 +0530

    Second commit

--- MergeSortedQ1.cpp
+++ MergeSortedQ1.cpp
@@ -1,30 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> merge(vector<int> v1,vector<int> v2){
-        vector<int> ans(v1.size() + v2.size());
-        int i=0,j=0,k=0;
-        while(i<v1.size() && j<v2.size()){
-            if(v1[i] <= v2[j]) ans[k++] = v1[i++];
-            else ans[k++] = v2[j++];
+void merge(vector<int> &v1,vector<int> &v2,int n,int m){
+        int i = n-1,j=m-1,k=n+m-1;
+        while(i>=0 && j>=0){
+            if(v1[i] >= v2[j]) v1[k--]=v1[i--];
+            else v1[k--]=v2[j--];
         }
-        while( i < v1.size()) ans[k++]= v1[i++]; 
-        while( j < v2.size()) ans[k++] = v2[j++];
-        return ans;
+        while(j>=0) v1[k--]=v2[j--];
+        return;
 }
 
 int main(){
     int n , m;
     cout<<"Enter value of n and m:";
     cin>>n>>m;
-    vector<int> v1(n),v2(m);
-    cout<<"Enter elements of vector 1:";
+    vector<int> v1(n+m,0),v2(m);
+    cout<<"Enter elements of vector 1 in sorted order:";
     for(int i=0 ;i<n;i++) cin>>v1[i];
-    cout<<"Enter elements of vector 2:";
+    cout<<"Enter elements of vector 2 in sorted order:";
     for(int i=0;i<m;i++) cin>>v2[i];
-    sort(v1.begin(),v1.end());
-    sort(v2.begin(),v2.end());
-    v1 = merge(v1,v2);
+    merge(v1,v2,n,m);
     cout<<"The merged array is :";
     for(int i : v1) cout<<i<<" ";
     cout<<endl;
