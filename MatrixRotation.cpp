175c4c45eb17e0ff64ccfb6962d50361201ba7fb
Prrofessor <divyanshyadav23457@gmail.com>
2023-01-21 17:57:18 +0530

    First Commit

--- MatrixRotation.cpp
+++ MatrixRotation.cpp
@@ -37,9 +37,11 @@ void rotate(int **arr,int n,int m){
 
 int main(){
     int n,m;
+    cout<<"Enter number of rows and columns:";
     cin>>n>>m;
     int **arr =new int*[n];
     for(int i=0;i<n;i++) arr[i]=new int[m];
+    cout<<"Enter the values of the matrix :"; 
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++)
             cin>>arr[i][j];
