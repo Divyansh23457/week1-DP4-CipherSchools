d2aeebf7c2cb1a1124505c4e375847190fbcab30
Divyansh Yadav <92618414+Prrofessor@users.noreply.github.com>
2023-01-25 01:44:33 +0530

    Update DetectAndRemoveLoop.cpp

--- DetectAndRemoveLoop.cpp
+++ DetectAndRemoveLoop.cpp
@@ -1,38 +1,152 @@
 /*
-Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
-There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
-Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. 
-Note that pos is not passed as a parameter.
-Do not modify the linked list.
-*/
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
+Given a linked list of N nodes such that it may contain a loop.
+A loop here means that the last node of the link list is connected to the node at position X(1-based index). 
+If the link list does not have any loop, X=0.
+Remove the loop from the linked list, if it is present, i.e. unlink the last node which is forming the loop.
  */
-class Solution {
-public:
-    ListNode *detectCycle(ListNode *head) {
-        if(!head || !head->next) return NULL;
-        ListNode *fast = head;
-        ListNode *slow = head;
-        while(fast && fast->next){
-            fast = fast->next->next;
-            slow = slow->next;
-            if(fast == slow){
-                break;
-            }
-        }
+//{ Driver Code Starts
+// driver code
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+bool isLoop(Node* head)
+{
+    if(!head) return false;
+    
+    Node* fast = head->next;
+    Node* slow = head;
+    
+    while( fast != slow)
+    {
+        if( !fast || !fast->next ) return false;
+        fast=fast->next->next;
+        slow=slow->next;
+    }
+    
+    return true;
+}
+
+int length(Node* head)
+{
+    int ret = 0;
+    while(head)
+    {
+        ret++;
+        head = head->next;
+    }
+    return ret;
+}
+
+
+// } Driver Code Ends
+/*
+structure of linked list node:
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+*/
+class Solution
+{
+    public:
+    Node* detectloop(Node *head){
+     Node * fast = head,*slow = head;
+     while(fast && fast->next){
+        fast = fast->next->next;
+        slow = slow->next;
+        if(fast == slow) break;
+     }
         if(!fast) return NULL;
-        slow =head;
-        while(fast && fast != slow) {
+        slow = head;
+        while(slow != fast) {
             slow = slow->next;
             fast = fast->next;
         }
-        
         return fast;
     }
+    
+    void removeLoop(Node* head)
+    {
+       Node *fast=head,*slow=head;
+        while(fast && fast->next){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow) break;
+        }
+        if(fast!=slow) return;
+        slow=head;
+        while(slow!=fast) {
+            slow=slow->next;
+            fast=fast->next;
+        }
+        while(fast->next!=slow) fast=fast->next;
+        fast->next=NULL;
+        return ;
+    }
 };
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        Solution ob;
+        ob.removeLoop(head);
+        
+        if( isLoop(head) || length(head)!=n )
+            cout<<"0\n";
+        else
+            cout<<"1\n";
+    }
+	return 0;
+}
